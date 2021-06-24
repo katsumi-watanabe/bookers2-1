1648e38a0ecec2ea8f3129f2db53eee43a7e240b
katsumi-watanabe <ktm981124@gmail.com>
2021-06-24 14:42:06 +0000

    tenth commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '96ab591ba56a2d969bd2cef74d1e17615484030943667ca50fa5238818df85520bfba8505dc8c57a875430d64428866308e39f7ea939f0affcde1501411d91f4'
